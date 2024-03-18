5756635477b08b2ec051a02e4c0a01f0e3450b6b
s28502 <s28502@pjwstk.edu.pl>
2024-03-18 02:11:45 +0100

    implementacja metoda max

--- Zaj_2/Zaj_2/Program.cs
+++ Zaj_2/Zaj_2/Program.cs
@@ -15,14 +15,15 @@ public class Program
         Console.WriteLine("Średnia dla tablicy {1,2,3,4,2,3} to: " + CalculateAverage(new []{1,2,3,4,2,3}));
         //String a string
         
-        
+        Console.WriteLine("Max wartosc dla tablicy {1,2,3,4,2,3} to: " + Max(new []{1,2,3,4,2,3}));
+
     }
 
     public static double CalculateAverage(int[] numbers)
     {
         if (numbers == null || numbers.Length == 0)
         {
-            Console.WriteLine("Pusta tablica lub null");
+            Console.WriteLine("Array cannot be null or empty");
             return 0;
         }
 
@@ -33,5 +34,25 @@ public class Program
         }
 
         return sum / numbers.Length;
+    }
+
+    public static int Max(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            Console.WriteLine("Array cannot be null or empty");
+            return 0;
+        }
+
+        int max = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (max < numbers[i])
+            {
+                max = numbers[i];
+            }
+        }
+
+        return max;
     }
 }
